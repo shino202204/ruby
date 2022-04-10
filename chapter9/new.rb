a9c3bdbfb762657d12582934be41388f7f342a3d
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-04-10 23:17:51 +0000

    [Add]new.rbにコードを追加

---
+++ chapter9/new.rb
@@ -0,0 +1,14 @@
+class Car
+  def turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+car = Car.new
+car.turn("右")
+car.run(5)
+
+# Rubyでは全てのデータをオブジェクトとして扱います。
