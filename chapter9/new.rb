a54581e82f52880a6d26a27e39458ff21361838b
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-04-11 03:22:09 +0000

    [Add]演習問題でクラスメソッドを追加

--- chapter9/new.rb
+++ chapter9/new.rb
@@ -1,14 +1,31 @@
+# Rubyでは全てのデータをオブジェクトとして扱います。
+
+# インスタンスメソッド
 class Car
+
+  def move(direction, distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+
   def turn(direction)
     puts "#{direction}に曲がります。"
   end
+
   def run(distance)
     puts "車で#{distance}キロ走ります。"
   end
+
+# car = Car.new
+# car.move("右", 5)
+
+# クラスメソッド
+  def self.turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+
 end
 
-car = Car.new
-car.turn("右")
-car.run(5)
 
-# Rubyでは全てのデータをオブジェクトとして扱います。
+# 呼び出し
+Car.turn("右")
