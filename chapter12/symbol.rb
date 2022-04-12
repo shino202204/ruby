83d7da477819a41e6109d4b59a672bc6bc5f239f
shino202204 <102901960+shino202204@users.noreply.github.com>
2022-04-12 03:32:47 +0000

    [Add]symbol.rbを追加

---
+++ chapter12/symbol.rb
@@ -0,0 +1,18 @@
+# シンボルは書き換え不可
+webcamp =:プログラミング学習
+puts webcamp
+
+#文字列オブジェクト
+dog1 = "犬"
+dog2 = "犬"
+
+#シンボルオブジェクト
+dog3 =:犬
+dog4 =:犬
+
+#シンボルは一意性がある
+# シンボルはしょりが高速
+puts dog1.object_id
+puts dog2.object_id
+puts dog3.object_id
+puts dog4.object_id
